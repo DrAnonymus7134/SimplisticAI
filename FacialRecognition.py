2868fb9ecd26bf7198c6100e34895ce2a30ef694
Dr. Anonymus <132111430+DrAnonymus7134@users.noreply.github.com>
2023-11-18 13:14:18 -0800

    FacialRecognition.py

--- FacialRecognition.py
+++ FacialRecognition.py
@@ -1,5 +1,5 @@
 import cv2
-width = 640
+width = 640 # Change this to your preferred width
 height = int(width * 0.5625)
 
 cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
@@ -8,7 +8,7 @@ cam.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 cam.set(cv2.CAP_PROP_FPS, 60)
 cam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
 
-faceCascade = cv2.CascadeClassifier('Haar\haarcascade_frontalface_default.xml')
+faceCascade = cv2.CascadeClassifier('Haar\haarcascade_frontalface_default.xml') # You might need to change this to the path where you put the file
 
 while True:
     ignore, frame = cam.read()
@@ -20,7 +20,7 @@ while True:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         
     cv2.imshow('my Webcam', frame)
-    cv2.moveWindow('my Webcam', 0, 0)
+    cv2.moveWindow('my Webcam', 0, 0) # Change these last two digits to where you want the picture
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
